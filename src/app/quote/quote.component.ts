e12a3a8e09de9b8d62a20f017036e76889f6d0d6
Benardakaka <benardakaka484@gmail.com>
2020-11-07 15:23:46 +0300

    add

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -14,7 +14,7 @@ export class  QuoteComponent implements OnInit {
     new Quote(3,'FRANCIA MBUGUA',"“When you educate one person you can change a life, when you educate many you can change the world if you are planning for a lifetime, educate”",new Date(2020,10,17)),
      new Quote(4,'LAVENDAH AKAKA', " “The beautiful thing about learning is that no one can take it away from you ” ",new Date(2020,12,21)),
      new Quote(2,'VIOLET ASEKA',"“If You are planning for a year, sow rice; if you are planning for a decade, plant trees; if you are planning for a lifetime, educate people”",new Date(2020,10,20)),
-    new Quote(3,'KERRY KOGEI',"“When you educate one person you can change a life, when you educate many you can change the world if you are planning for a lifetime, educate”",new Date(2020,12,30)),
+    new Quote(3,'KERRY KOGEI',"“When you Educate one person you can change a life, when you educate many you can change the world if you are planning for a lifetime, educate”",new Date(2020,12,30)),
   
   ];  
  
